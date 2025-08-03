799dfda32bb4e5f0772d4b78e9e56ffeb151ae97
Maheswara Rivai Jamhur <613220032@std.ulbi.ac.id>
2025-08-03 21:21:44 +0000

    coba buatkan akun mockup pengurus rw aja agar bisa ke dashbord pengurus

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -48,6 +48,15 @@ let mockUsers: User[] = [
         role: "Petugas RT/RW",
         rtRw: "002/001",
         avatarUrl: "https://placehold.co/100x100.png?text=D",
+      },
+      {
+        id: "user-5",
+        name: "Eka Wulandari",
+        email: "eka@desa.com",
+        password_hash: '$2a$10$wOK.i4yJZRcM3G9V4o/C8.LhJMAZH1DltJBPpAEKjVz5tO3O4ISiG', // Hashed "password123"
+        role: "Petugas RT/RW",
+        rtRw: "003/001",
+        avatarUrl: "https://placehold.co/100x100.png?text=E",
       }
 ];
 
